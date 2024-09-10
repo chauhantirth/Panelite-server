acfea07c4352b07577b3eef372f4d0e3c12c8950
chauhantirth <chauhantirth40@gmail.com>
2024-09-10 12:55:39 +0530

    Add: userData builder function

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, jsonify, request
 import toml
 import os
 from pathlib import Path
+import db
+import base64
 
 BASE_DIR = Path(os.getcwd())
 
@@ -9,8 +11,22 @@ with open(BASE_DIR / 'config.toml', 'r') as config_file:
     config = toml.load(config_file)
     API_PORT = config.get('API_PORT')
     API_HOST = config.get('API_HOST')
+    IS_DEV = config.get('IS_DEVELOPMENT')
 
-print(config)
+def getUserData(user):
+    userData = user.copy()
+    userData.pop('userAuthDetails', None)
+
+    for org in db.org_details['orgs']:
+        if org['orgId'] == user['associatedOrgId']:
+            userData['orgName'] = org['orgName']
+            userData['orgNameShort'] = org['orgNameShort']
+            userData['orgLocation'] = org['orgLocation']
+            userData['orgCity'] = org['orgCity']
+            userData['plantCapacity'] = org['orgPlantDetails']['plantCapacity']
+            userData['plantNumberOfPanels'] = org['orgPlantDetails']['plantNumberOfPanels']
+
+    return base64.b64encode(str(userData).encode()).decode()
 
 app = Flask(__name__)
 
@@ -18,15 +34,16 @@ app = Flask(__name__)
 def refresh():
     try:
         json_data = request.get_json()
+        sessionToken = json_data['data']['sessionToken']
 
-        if json_data['data']['sessionToken'] not in ['token1', 'token2']: 
-            return jsonify({'status': 'failure', 'errorMsg': 'Invalid Token, Please Login Again.'})
-        
-        else:
-            return jsonify({'status': 'success', 'container': {
-                'sessionToken': json_data['sessionToken'],
-                'userData': 'eyJ1c2VySWQiOiIwNmQ2MDdlZC04NTRiLTRkZWYtYTVjYy1jMzYwNjI3NzYxM2YiLCJvcmdJZCI6IjA1ODQxMjRkLWE1OTgtNDkzYi04ZGM2LTM1ZDRiOGRiZDJiYyIsIm9yZ05hbWUiOiJSLiBOLiBHLiBQYXRlbCBJbnN0aXR1dGUgT2YgVGVjaG5vbG9neSIsIm9yZ05hbWVTaG9ydCI6IlJOR1BJVCIsIm9yZ0xvY2F0aW9uIjoiUk5HUElULCBCYXJkb2xpLCBHdWphcmF0LCBJbmRpYSIsIm9yZ0NpdHkiOiJCYXJkb2xpIiwicGxhbnRDYXBhY2l0eSI6IjEyMCBLd2giLCJwbGFudE51bWJlck9mUGFuZWxzIjoiMTgwIiwidXNlck5hbWUiOiJEZXYgVXNlciIsInVzZXJFbWFpbCI6ImRldjAwQHBhbmVsaXRlLmNvbSIsInVzZXJNb2JpbGUiOiIrOTE4KioqKiowMzIzIn0='
-            }})
+        for user in db.user_details['users']:
+            if sessionToken == user['userAuthDetails']['sessionToken']:
+                return jsonify({'status': 'success', 'container': {
+            'sessionToken': user['userAuthDetails']['sessionToken'],
+            'userData': getUserData(user)
+        }})
+
+        return jsonify({'status': 'failure', 'errorMsg': 'Invalid Token, Please Login Again.'})
 
     except Exception as error:
         print(error)
@@ -34,4 +51,4 @@ def refresh():
 
 # Run the app if this script is executed
 if __name__ == '__main__':
-    app.run(API_HOST, API_PORT, debug=config.get('IS_DEVELOPMENT'))
+    app.run(API_HOST, API_PORT, debug=config.get('IS_DEVELOPMENT'), use_reloader = True)
