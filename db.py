744c8287f579004c211099dffda7fe06a66d82d2
chauhantirth <chauhantirth40@gmail.com>
2024-09-10 13:04:08 +0530

    Add: userPassword field

--- db.py
+++ db.py
@@ -6,17 +6,19 @@ user_details = {
             'userFirstName': 'Tirth',
             'userLastName': 'Chauhan',
             'userEmail': 'tirthchauhan@panelite.com',
+            'userPassword': 'Panelite@123',
             'userMobile': '+919*****2132',
             'userAuthDetails': {
                 'sessionToken': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJPbmxpbmUgSldUIEJ1aWxkZXIiLCJpYXQiOjE3MjU5NTA2OTEsImV4cCI6MTc1NzQ4NjY5MSwiYXVkIjoid3d3LmV4YW1wbGUuY29tIiwic3ViIjoianJvY2tldEBleGFtcGxlLmNvbSIsInVzZXJGaXJzdE5hbWUiOiJUaXJ0aCIsInVzZXJMYXN0TmFtZSI6IkNoYXVoYW4iLCJ1c2VyRW1haWwiOiJ0aXJ0aGNoYXVoYW5AcGFuZWxpdGUuY29tIiwidXNlck1vYmlsZSI6Iis5MTkqKioqKjIxMzIiLCJ1c2VySWQiOiIwNmQ2MDdlZC04NTRiLTRkZWYtYTVjYy1jMzYwNjI3NzYxM2YiLCJhc3NvY2lhdGVkT3JnSWQiOiIwNTg0MTI0ZC1hNTk4LTQ5M2ItOGRjNi0zNWQ0YjhkYmQyYmMifQ.-A7F9cqlreIG1vscJkleg1tjqfvR1uXYkCMb338wgHk',
             },
         },
-            {
+        {
             'userId': '761e1e70-55af-4628-84b5-72f58d128e0a',
             'associatedOrgId': '0584124d-a598-493b-8dc6-35d4b8dbd2bc',
             'userFirstName': 'Dev',
             'userLastName': 'User',
             'userEmail': 'devuser@panelite.com',
+            'userPassword': 'Panelite@123',
             'userMobile': '+918*****4425',
             'userAuthDetails': {
                 'sessionToken': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJPbmxpbmUgSldUIEJ1aWxkZXIiLCJpYXQiOjE3MjU5NTA2OTEsImV4cCI6MTc1NzQ4NjY5MSwiYXVkIjoid3d3LmV4YW1wbGUuY29tIiwic3ViIjoianJvY2tldEBleGFtcGxlLmNvbSIsInVzZXJGaXJzdE5hbWUiOiJEZXYiLCJ1c2VyTGFzdE5hbWUiOiJVc2VyIiwidXNlckVtYWlsIjoiZGV2dXNlckBwYW5lbGl0ZS5jb20iLCJ1c2VyTW9iaWxlIjoiKzkxOCoqKioqNDQyNSIsInVzZXJJZCI6Ijc2MWUxZTcwLTU1YWYtNDYyOC04NGI1LTcyZjU4ZDEyOGUwYSIsImFzc29jaWF0ZWRPcmdJZCI6IjA1ODQxMjRkLWE1OTgtNDkzYi04ZGM2LTM1ZDRiOGRiZDJiYyJ9.Ch2qenF34oNyg3Mn9WZ19KPSPeQ0CqO6v92Z2jGSizM',
